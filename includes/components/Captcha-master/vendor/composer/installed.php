02607fd8df3069ffdfdcc3f2d345c1135cf25471
Ederson Alves da Silva <46879808+edhaki@users.noreply.github.com>
2023-10-22 22:46:24 -0300

    Add files via upload

---
+++ includes/components/Captcha-master/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'gregwar/captcha',
+  ),
+  'versions' => 
+  array (
+    'gregwar/captcha' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'symfony/finder' => 
+    array (
+      'pretty_version' => 'v5.3.0',
+      'version' => '5.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0ae3f047bed4edff6fd35b26a9a6bfdc92c953c6',
+    ),
+  ),
+);
