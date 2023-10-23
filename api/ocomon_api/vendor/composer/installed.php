4e6bfbbfaf0da446398f9a80975fc351017b571d
Ederson Alves da Silva <46879808+edhaki@users.noreply.github.com>
2023-10-22 23:20:13 -0300

    Add files via upload

---
+++ api/ocomon_api/vendor/composer/installed.php
@@ -0,0 +1,69 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '06a2e5ccf6aa46da2162716b371bf708f100ea65',
+    'name' => 'flaviorib-dev/ocomon_api',
+  ),
+  'versions' => 
+  array (
+    'coffeecode/datalayer' => 
+    array (
+      'pretty_version' => '1.1.8',
+      'version' => '1.1.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '52e209190752eccc0bfd8ff3abcdd607ed4c01c1',
+    ),
+    'coffeecode/paginator' => 
+    array (
+      'pretty_version' => '1.0.7',
+      'version' => '1.0.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a6941246f7ccd842be2072d796d4fb448c22cdb1',
+    ),
+    'coffeecode/router' => 
+    array (
+      'pretty_version' => '1.0.8',
+      'version' => '1.0.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '29f73e12351a116e9bbb3f695595777a09e18077',
+    ),
+    'firebase/php-jwt' => 
+    array (
+      'pretty_version' => 'v5.2.1',
+      'version' => '5.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f42c9110abe98dd6cfe9053c49bc86acc70b2d23',
+    ),
+    'flaviorib-dev/ocomon_api' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '06a2e5ccf6aa46da2162716b371bf708f100ea65',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.2.0',
+      'version' => '6.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e38888a75c070304ca5514197d4847a59a5c853f',
+    ),
+  ),
+);
